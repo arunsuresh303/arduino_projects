411479a0c2bbe3b1182455b9b28be30d72b4da2e
Arun Suresh <arun.suresh.303@gmail.com>
2020-12-21 11:31:03 +0530

    Removed commented block
    Change-Id: I0454bf3d011ff6224cf1bf453da74692e8fe4b2d
    Signed-off-by: Arun Suresh<arun.suresh.303@gmail.com>

--- ArduinoUno/MotorShieldTest/MotorShieldTest.ino
+++ ArduinoUno/MotorShieldTest/MotorShieldTest.ino
@@ -110,37 +110,3 @@ void doCounterClockWiseTurn() {
 
 }
 
-//int i;
-//
-//// Test the DC motor, stepper and servo ALL AT ONCE!
-//void loop() {
-//  motor.run(FORWARD);
-//  for (i=0; i<255; i++) {
-//    servo1.write(i);
-//    motor.setSpeed(i);
-//    stepper.step(1, FORWARD, INTERLEAVE);
-//    delay(3);
-// }
-//
-//  for (i=255; i!=0; i--) {
-//    servo1.write(i-255);
-//    motor.setSpeed(i);
-//    stepper.step(1, BACKWARD, INTERLEAVE);
-//    delay(3);
-// }
-//
-//  motor.run(BACKWARD);
-//  for (i=0; i<255; i++) {
-//    servo1.write(i);
-//    motor.setSpeed(i);
-//    delay(3);
-//    stepper.step(1, FORWARD, DOUBLE);
-// }
-//
-//  for (i=255; i!=0; i--) {
-//    servo1.write(i-255);
-//    motor.setSpeed(i);
-//    stepper.step(1, BACKWARD, DOUBLE);
-//    delay(3);
-// }
-//}
